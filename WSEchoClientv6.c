7c1482a359c775667928b3d0e7c15b04b057aa37
PhysicistsOfDoom <corbin.beus@gmail.com>
2025-10-25 21:15:13 -0600

    commit README

---
+++ WSEchoClientv6.c
@@ -0,0 +1,11 @@
+
+// Check command line arguments first
+// initialize the Winsock socket
+// Create a TCP socket (IPv6)
+// Set up the server address
+// Connect to the server
+// Send the echo
+// Receive the echo
+// Print the echo message
+// Close connection (End TCP handshake with FIN - ACK ... ACK)
+// Clean up!
